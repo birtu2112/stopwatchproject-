e9fab260eb0b4d5e0722435280ab9b71e3ff344e
birtu2112 <168409356+birtu2112@users.noreply.github.com>
2024-12-17 00:47:11 -0800

    Add files via upload

---
+++ Stopwatch project.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var stopwatch = new Stopwatch();
+
+        stopwatch.OnStarted += message => Console.WriteLine(message);
+        stopwatch.OnStopped += message => Console.WriteLine(message);
+        stopwatch.OnReset += message => Console.WriteLine(message);
+
+        Console.WriteLine("Press S to start, T to stop, or R to reset the stopwatch.");
+
+        while (true)
+        {
+            var key = Console.ReadKey(true).Key;
+            switch (key)
+            {
+                case ConsoleKey.S:
+                    stopwatch.Start();
+                    break;
+                case ConsoleKey.T:
+                    stopwatch.Stop();
+                    break;
+                case ConsoleKey.R:
+                    stopwatch.Reset();
+                    break;
+            }
+
+            if (stopwatch.IsRunning)
+            {
+                Console.WriteLine($"Time Elapsed: {stopwatch.TimeElapsed}");
+            }
+        }
+    }
+}
---
+++ program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading;
+public class Stopwatch
+{
+    private TimeSpan timeElapsed;
+    private bool isRunning;
+
+    public delegate void StopwatchEventHandler(string message);
+    public event StopwatchEventHandler OnStarted;
+    public event StopwatchEventHandler OnStopped;
+    public event StopwatchEventHandler OnReset;
+
+    public TimeSpan TimeElapsed => timeElapsed;
+    public bool IsRunning => isRunning;
+
+    public void Start()
+    {
+        if (!isRunning)
+        {
+            isRunning = true;
+            OnStarted?.Invoke("Stopwatch Started!");
+            Run();
+        }
+    }
+
+    public void Stop()
+    {
+        if (isRunning)
+        {
+            isRunning = false;
+            OnStopped?.Invoke("Stopwatch Stopped!");
+        }
+    }
+
+    public void Reset()
+    {
+        isRunning = false;
+        timeElapsed = TimeSpan.Zero;
+        OnReset?.Invoke("Stopwatch Reset!");
+    }
+
+    private void Run()
+    {
+        while (isRunning)
+        {
+            Thread.Sleep(1000);
+            timeElapsed = timeElapsed.Add(TimeSpan.FromSeconds(1));
+        }
+    }
+};
+
